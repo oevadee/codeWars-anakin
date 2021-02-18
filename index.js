b370184e5c3ea33c20cdcbd0a69e8081a783b1b8
Adrian Szczechura <a.szczechura.98@gmail.com>
2021-02-18 23:30:50 +0100

    finished 3rd kata, gonna take a break for today

--- index.js
+++ index.js
@@ -15,11 +15,29 @@ const grow = (x) => x.reduce((curr, acc) => curr * acc);
 // 25  =>  true
 // 26  =>  false
 
-const isSquare = (x) => Math.sqrt(x) % parseInt(Math.sqrt(x)) === 0 || x === 0 ? true : false;
+const isSquare = (x) =>
+  Math.sqrt(x) % parseInt(Math.sqrt(x)) === 0 || x === 0 ? true : false;
 
-console.log(isSquare(-1));
-console.log(isSquare(0));
-console.log(isSquare(3));
-console.log(isSquare(4));
-console.log(isSquare(25));
-console.log(isSquare(26));
+// console.log(isSquare(-1));
+// console.log(isSquare(0));
+// console.log(isSquare(3));
+// console.log(isSquare(4));
+// console.log(isSquare(25));
+// console.log(isSquare(26));
+
+const getCount = (str) => {
+  let vowelsCount = 0;
+  str
+    .split("")
+    .map((letter) =>
+      ["a", "e", "i", "o", "u"].includes(letter) ? vowelsCount++ : vowelsCount
+    );
+  return vowelsCount;
+};
+
+// console.log(getCount("abracadabra"));
+
+const getSum = (a, b) => {};
+
+console.log(getSum(0, -1));
+console.log(getSum(0, 1));
